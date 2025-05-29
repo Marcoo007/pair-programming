f6a10f4c9055ded7678f0c47e144fd6161f523b8
Marco Wismer <marco_wismer@sluz.ch>
2025-05-29 22:52:03 +0200

    abzüge gemacht

---
+++ src/payroll.test.ts
@@ -0,0 +1,5 @@
+//ein 16 jähriger Lernender mit einem Monatsgehalt von 700.-
+
+//ein 18 jähriger Lernender mit einem Monatsgehalt von 1200.-
+
+//ein 21 jähriger Angestellter mit einem Monatsgehalt von 5900.-
--- src/payroll.ts
+++ src/payroll.ts
@@ -24,6 +24,42 @@ export type Payslip = {
 
 export function calculatePayslip(salary: Salary): Payslip {
   // TODO: implement
+
+  const deductions: Deductions = new Map();
+  const ageAtPayday = 0;
+  const monthlyGross = salary.gross;
+  const yearlyGross = monthlyGross * 12;
+  //AHV, IV und EO werden ab dem 1. Januar nach dem 17. Geburtstag abgezogen.
+
+  const janAfter17 = new Date(salary.born.getFullYear() + 18, 0, 1);
+  if (salary.payday >= janAfter17) {
+    ["AHV", "IV", "EO"].forEach((key) => {
+      const rate = DEDUCTION_RATES.get(key)!;
+      deductions.set(key, monthlyGross * (rate / 100));
+    });
+  }
+
+  //ALV und NBU werden ab einem Jahreslohn(!) von 2'500.- abgezogen.
+
+  if (yearlyGross >= 2500) {
+    ["ALV", "NBU"].forEach((key) => {
+      const rate = DEDUCTION_RATES.get(key)!;
+      deductions.set(key, monthlyGross * (rate / 100));
+    });
+  };
+
+
+  //Pensionskasse (PK) wird ab einem Jahreslohn von 22'680.- abgezogen.
+  if (yearlyGross >= 22680) {
+    const rate = DEDUCTION_RATES.get("PK")!;
+    deductions.set("PK", monthlyGross * (rate / 100));
+  };
+  
+  let totalDeductions = 0;
+  deductions.forEach((value) => {
+    totalDeductions += value;
+  });
+
   const result: Payslip = {
     salary: salary,
     deductions: new Map(),
